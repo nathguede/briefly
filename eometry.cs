e643d3ec16e6599bc54b743a6dfdae738b03d9e1
yuramazurkevich <52805721+yuramazurkevich@users.noreply.github.com>
2025-09-28 01:49:08 +0300

    Update eometry.cs

--- eometry.cs
+++ eometry.cs
@@ -1,1 +1,33 @@
-eometry.sln
+﻿
+using ConsoleAppGeometry.Shape;
+
+namespace ConsoleAppGeometry;
+
+class Program
+{
+    public static int Main(string[] args)
+    {
+        string shapeType = Console.ReadLine() ?? string.Empty;
+        IShape shape;
+
+        switch(shapeType)
+        {
+            case "t":
+                shape = new Triangle(Utils.ReadPositiveInt());               
+                break;
+            case "r":
+                shape = new Rectangle(Utils.ReadPositiveInt(), Utils.ReadPositiveInt());
+                break;
+            case "s":
+                shape = new Square(Utils.ReadPositiveInt());
+                break;
+            default:
+                Console.WriteLine("Wrong shape type, must be one of: t, r, s");
+                return 1;
+        }
+
+        shape.Print();
+
+        return 0;
+    }
+}
